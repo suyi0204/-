d2902307c2e8590adfdca07b9038991786b8dfb9
suyi._17 <145542867+suyi0204@users.noreply.github.com>
2025-11-16 21:37:22 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // 中間件 - 修正 CORS
 app.use(express.json());
